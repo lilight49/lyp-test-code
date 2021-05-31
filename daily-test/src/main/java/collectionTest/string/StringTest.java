fd85e135cb157e6a5ca88caf3bffedf07a6a3a03
liyunpeng <15235688150@163.com>
2021-05-31 17:36:08 +0800

    record

--- daily-test/src/main/java/collectionTest/string/StringTest.java
+++ daily-test/src/main/java/collectionTest/string/StringTest.java
@@ -1,11 +1,19 @@
 package collectionTest.string;
 
+import io.netty.handler.codec.http.HttpHeaderValues;
+
 import javax.xml.transform.Source;
 
 public class StringTest {
     public static void main(String[] args) {
 
-        String b = "1234343";
-        System.out.println(b.substring(0,2));
+//        String[] a = new String[2];
+//        a[0] = "ab";
+//        a[1] = "ce";
+//        a[2] = "df";
+//
+//        System.out.println(a.toString());
+
+        System.out.println(HttpHeaderValues.APPLICATION_X_WWW_FORM_URLENCODED.toString());
     }
 }
